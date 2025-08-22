a99f157a5ad90a80a76e8470dc53d3011028a44a
Yashwant-Ahirwar <yashoo.kumar@gmail.com>
2025-08-22 20:05:11 +0530

    first commit

---
+++ src/main/java/com/example/app/entity/User.java
@@ -0,0 +1,17 @@
+package com.example.app.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Table(name = "users")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(nullable = false)
+    private String name;
+}
